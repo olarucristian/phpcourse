d522367bd2c81fb8abfeb9ef082b024ce61670af
olarucristian <olarucristi1995@gmail.com>
2018-11-27 13:24:09 +0200

    chapter 6 done

--- 6.php
+++ 6.php
@@ -2,28 +2,49 @@
 <?php include "functions.php"; ?>
 <?php include "includes/header.php";?>
 
-	<section class="content">
+<section class="content">
 
-		<aside class="col-xs-4">
-		
-		<?php Navigation();?>
-			
-		</aside><!--SIDEBAR-->
+    <aside class="col-xs-4">
 
+        <?php Navigation();?>
 
-<article class="main-content col-xs-8">
- 
-
-	<?php  
-
-/*  Step1: Make a form that submits one value to POST super global
+    </aside><!--SIDEBAR-->
 
 
- */
+    <article class="main-content col-xs-8">
+        <!--  Make a form that submits one value to POST super global  -->
+        <form action = "6.php" method="post">
+            <input type="text" placeholder="Username" name="username">
+            <input type="password" placeholder="Password" name="password">
+            <br>
+            <input type="submit" name="submit">
+        </form>
 
-	
-?>
+
+        <?php
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+        $users = ['Martin', 'Cristi', 'Igor'];
+
+        if (isset($_POST['submit'])){
+
+            //echo "Hello $username your password is $password";
+        }
+
+        if (strlen($username ) < 5){
+            echo "Username has to be longer than 5 characters";
+
+        }
+
+        if (!in_array($username, $users)){
+            echo "You are not allowed";
+        }else{
+            echo "Welcome";
+        }
 
 
-</article><!--MAIN CONTENT-->
+        ?>
+
+
+    </article><!--MAIN CONTENT-->
 <?php include "includes/footer.php"; ?>
