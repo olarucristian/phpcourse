ba46cebfeb78ccac43fc15cb64563c095ee0f276
olarucristian <olarucristi1995@gmail.com>
2018-11-29 11:24:09 +0200

    chapter 7 done

--- 7.php
+++ 7.php
@@ -1,4 +1,21 @@
 <?php include "functions.php"; ?>
+
+
+<?php
+$connection = mysqli_connect('localhost', 'root', 'admin','mylist');
+
+if(!$connection){
+    die("Database connection failed") . mysqli_error($connection);
+
+}
+$query = "SELECT * FROM reports";
+$result = mysqli_query($connection, $query);
+if (!$result){
+    die("Query Failed");
+}
+
+?>
+
 <?php include "includes/header.php";?>
     
 
@@ -27,6 +44,12 @@
 		Step 4 - Connect to Database and read data
 
 */
+
+
+
+	while ($record = mysqli_fetch_assoc($result)) {
+	    echo $record['days_of_the_week'];
+    }
 	
 	?>
 
