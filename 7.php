e8c3918d5fb972b93c4043fb25b68c459fe5fde1
olarucristian <olarucristi1995@gmail.com>
2018-11-29 11:29:38 +0200

    chapter 7 done

--- 7.php
+++ 7.php
@@ -17,46 +17,46 @@ if (!$result){
 ?>
 
 <?php include "includes/header.php";?>
-    
-
-	<section class="content">
-
-		<aside class="col-xs-4">
-
-		<?php Navigation();?>
-			
-			
-		</aside><!--SIDEBAR-->
 
 
-	<article class="main-content col-xs-8">
-	
-	
-	
-	<?php  
+<section class="content">
 
-	/*  Step 1 - Create a database in PHPmyadmin
+    <aside class="col-xs-4">
 
-		Step 2 - Create a table like the one from the lecture
+        <?php Navigation();?>
 
-		Step 3 - Insert some Data
 
-		Step 4 - Connect to Database and read data
+    </aside><!--SIDEBAR-->
 
-*/
+
+    <article class="main-content col-xs-8">
 
 
 
-	while ($record = mysqli_fetch_assoc($result)) {
-	    echo $record['days_of_the_week'];
-    }
-	
-	?>
+        <?php
+
+        /*  Step 1 - Create a database in PHPmyadmin
+
+            Step 2 - Create a table like the one from the lecture
+
+            Step 3 - Insert some Data
+
+            Step 4 - Connect to Database and read data
+
+    */
+
+
+
+        while ($record = mysqli_fetch_assoc($result)) {
+            echo $record['days_of_the_week'];
+        }
+
+        ?>
 
 
 
 
 
-</article><!--MAIN CONTENT-->
+    </article><!--MAIN CONTENT-->
 
-<?php include "includes/footer.php"; ?>
+    <?php include "includes/footer.php"; ?>
