bc09c2b063200f488c7026fc1ddca4172f6c5482
olarucristian <olarucristi1995@gmail.com>
2018-12-06 11:09:18 +0200

    chapter 9 done

--- 9.php
+++ 9.php
@@ -1,37 +1,56 @@
 <?php include "functions.php"; ?>
 <?php include "includes/header.php";?>
+<?php
+session_start();
+$_SESSION['message'] = "Hello";
+
+$expiration = time() + (60*60*24*7);
+
+setcookie("Name","Value","$expiration");
+?>
+
+
+    <section class="content">
+
+    <aside class="col-xs-4">
+
+        <?php Navigation();?>
+
+
+    </aside><!--SIDEBAR-->
+
+
+    <article class="main-content col-xs-8">
 
 
 
-	<section class="content">
+        <?php
 
-		<aside class="col-xs-4">
+        /*  Create a link saying Click Here, and set
+        the link href to pass some parameters and use the GET super global to see it
 
-		<?php Navigation();?>
-			
-			
-		</aside><!--SIDEBAR-->
+            Step 2 - Set a cookie that expires in one week
 
+            Step 3 - Start a session and set it to value, any value you want.
+        */
 
-			<article class="main-content col-xs-8">
-			
-		
-	
-	<?php 
+        if (isset($_GET['source'])){
+            echo $_GET['source']. "<br>";
+        }
 
-	/*  Create a link saying Click Here, and set 
-	the link href to pass some parameters and use the GET super global to see it
-
-		Step 2 - Set a cookie that expires in one week
-
-		Step 3 - Start a session and set it to value, any value you want.
-	*/
-	
-	?>
-
-
+        ?>
+        <a href="9.php?source=12342">Click Here</a>
+        <?php
+        if (isset($_COOKIE["Name"])){
+            echo $_COOKIE["Name"];
+        }
+        $_SESSION['message'];
+        if (isset($_SESSION['message'])) {
+            echo $_SESSION['message'];
+        }
+        ?>
 
 
 
-</article><!--MAIN CONTENT-->
+    </article><!--MAIN CONTENT-->
 <?php include "includes/footer.php"; ?>
